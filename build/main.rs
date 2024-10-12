98e3021b9ae8c4cb05749872e40dedf4a8673942
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-10-12 18:13:25 -0400

    build: properly use `$OUT_DIR` as the destination for generated files

--- build/main.rs
+++ build/main.rs
@@ -17,36 +17,27 @@ type DynResult = Result<(), Box<dyn Error>>;
 
 fn main() -> DynResult {
     println!("cargo:rerun-if-changed=build/");
-    mangen()
+    let out_dir = Path::new(&env::var("OUT_DIR")?).to_path_buf();
+    mangen(out_dir)
 }
 
 /// Generate man page for binary and subcommands
-fn mangen() -> DynResult {
+fn mangen(out_dir: PathBuf) -> DynResult {
     println!("cargo:rerun-if-env-changed=PROJECT_NAME");
     println!("cargo:rerun-if-env-changed=PROJECT_EXECUTABLE");
     println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
 
     eprintln!("Generating man pages");
 
-    let out_dir = release_dir().join("manual/man1");
+    let dest_dir = out_dir.join("manual/man1");
     let cmd = Cmd::command().name("handlr");
 
-    create_dir_all(&out_dir)?;
+    create_dir_all(&dest_dir)?;
 
-    clap_mangen::generate_to(cmd, &out_dir)?;
+    clap_mangen::generate_to(cmd, &dest_dir)?;
 
     // Remove hidden subcommand's manpage
-    remove_file(out_dir.join("handlr-autocomplete.1"))?;
+    remove_file(dest_dir.join("handlr-autocomplete.1"))?;
 
     Ok(())
-}
-
-// Project root
-fn project_root() -> PathBuf {
-    Path::new(&env!("CARGO_MANIFEST_DIR")).to_path_buf()
-}
-
-/// Output directory for generated assets
-fn release_dir() -> PathBuf {
-    project_root().join("release")
 }
